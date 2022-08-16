f9e7f17ba43698c43d99e6d7f66f0f7439f445f8
Furhan Samani <contact@furhan.dev>
2022-08-16 13:16:11 -0700

    finish crud operations and update routes for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const { User, Thought } = require('../models/');
 
 module.exports = {
   getUsers(req, res) {
@@ -13,6 +13,47 @@ module.exports = {
         !user
           ? res.status(404).json({ message: 'No user with that ID' })
           : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .select('-__v')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : Thought.deleteMany(
+            { username: user.username },
+            { new: true }
+          )
+      )
+      .then((thoughts) =>
+        !thoughts
+          ? res
+            .status(404)
+            .json({ message: `User deleted but couldn't delete associated thoughts: ${JSON.stringify(thoughts)}!` })
+          : res.json({ message: 'User and associated thoughts successfully deleted!' })
       )
       .catch((err) => res.status(500).json(err));
   },
