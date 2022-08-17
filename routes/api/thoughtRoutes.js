d2a05302af7abfb285b346ab70583207659dd49d
Furhan Samani <contact@furhan.dev>
2022-08-17 13:22:00 -0700

    add thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,2 +1,29 @@
 const router = require('express').Router();
+
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction
+} = require('../../controllers/thoughtController');
+
+// /api/users
+router.route('/')
+  .get(getThoughts)
+  .post(createThought);
+
+// /api/users/:userId
+router.route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:thoughtId/reactions')
+  .post(addReaction)
+  .delete(deleteReaction);
+
 module.exports = router;
